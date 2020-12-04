ffefe53f984cb392c65ae7a498a8ceb999a6c8a0
Mogomotsi <mmoroane@hotmail.com>
2020-12-04 13:54:49 +0200

    Added links

--- app/src/main/java/com/example/guesscelebapp/MainActivity.java
+++ app/src/main/java/com/example/guesscelebapp/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.guesscelebapp;
 
+import android.os.AsyncTask;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url =new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                     char current = (char) data;
+                     result += current;
+                     data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+            result = task.execute("https://www.celebritynetworth.com/list/top-100-richest-people-in-the-world/").get();
+
+            Log.i("Content URL", result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
